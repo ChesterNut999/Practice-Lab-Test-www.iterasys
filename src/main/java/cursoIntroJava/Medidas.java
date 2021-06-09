ed81b645d9e51db15bbc0c815bc3f4497b78fc98
ChesterNut999 <maurilio_sjc@outlook.com>
2021-06-09 14:36:14 -0300

    Update_21_06_09

---
+++ src/main/java/cursoIntroJava/Medidas.java
@@ -0,0 +1,25 @@
+//1 - Pacote
+package cursoIntroJava;
+
+//2 - Referências as Bibliotecas
+
+
+//3 - Classe (Programação)
+public class Medidas {
+    //3.1 - Atributos da classe Medidas (caracteristicas da classe)
+
+
+    //3.2 - Métodos e funções (Como a classe executa ações)
+    public static void main (String[] args){
+        System.out.println("Olá Caralhoooo!");
+
+        //Calculo da área - Exemplo: o tamanho do tapete ou do piso
+        int largura = 4;
+        int comprimento = 5;
+        int resultado = largura*comprimento;
+
+        System.out.println("Para largura de " + largura +"m.");
+        System.out.println("Para comprimento de " + comprimento +"m.");
+        System.out.println("A área do tapete é de " + resultado +"m².");
+    }
+}
